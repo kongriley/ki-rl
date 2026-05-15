e30b6fa2e408a126026e33f1adbd0301d170bd59
Riley Kong <kongriley@gmail.com>
2026-05-15 06:56:37 +0000

    Add active learning CPT

--- baselines/cpt.sh
+++ baselines/cpt.sh
@@ -1,10 +1,11 @@
 NUM_GPUS=$(python -c "import torch; print(torch.cuda.device_count())")
 DATASET="thetech"
+MODE="active_reading" # cpt or active_reading
 
 torchrun --standalone --nproc_per_node=${NUM_GPUS} baselines/cpt.py \
     --model_name allenai/OLMo-2-1124-7B-Instruct \
-    --data_path data/${DATASET}/data.json \
-    --output_dir out/cpt_${DATASET} \
+    --data_path data/${DATASET}/$(if [ "${MODE}" = "cpt" ]; then echo "data"; else echo "${MODE}"; fi).json \
+    --output_dir out/${MODE}_${DATASET} \
     --block_size 2048 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
